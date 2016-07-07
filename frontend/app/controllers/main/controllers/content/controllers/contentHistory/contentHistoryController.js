2afe0b269762958727fdf1ca055fb4519b69b587
Zoltan Molnar <zoltan@momentum.ai>
2016-07-07 11:33:55 +0100

    campaigns part 1

--- frontend/app/controllers/main/controllers/content/controllers/contentHistory/contentHistoryController.js
+++ frontend/app/controllers/main/controllers/content/controllers/contentHistory/contentHistoryController.js
@@ -20,13 +20,118 @@ momentum.controller('ContentHistoryController', [
         $q,
         $timeout
     ) {
+        var defaultCampaigns = {
+            'data': [],
+            'sum': 0,
+            'currency': 'USD',
+            'offset': 0,
+            'cnt': 0
+        };
+
         $scope.viewLoaded = 0;
         $scope.statsDirty = 0;
 
         $scope.sourceStats = null;
+        $scope.content = {
+            'campaigns': null
+        };
+
+        $scope.toggle = function (id, active) {
+            var promise;
+
+            if (active) {
+                promise = dialog.open({
+                    'htmlText': 'Are you sure you ' +
+                        'want to stop this campaign?',
+                    'showCancel': true
+                }).then(function () {
+                    $scope.content.campaigns.loading = 1;
+                    return fb.disableCampaign(
+                        $scope.sessionId,
+                        id
+                    ).then(function () {
+                        storage.invalidateCache();
+                    });
+                });
+            } else {
+                promise = dialog.open({
+                    'htmlText': 'Are you sure you ' +
+                        'want to clone this campaign?',
+                    'showCancel': true
+                }).then(function () {
+                    $scope.content.campaigns.loading = 1;
+                    return fb.cloneCampaign(
+                        $scope.sessionId,
+                        id
+                    ).then(function () {
+                        storage.invalidateCache();
+                    });
+                });
+            }
+
+            promise.catch(function (err) {
+                if (typeof err === 'string') {
+                    return dialog.open({
+                        'htmlText': err
+                    });
+                }
+            });
+
+            promise.then(function () {
+                storage.invalidateCache();
+                return fb.getCampaigns(
+                    $scope.sessionId,
+                    $scope.stateParams.contentId,
+                    10,
+                    0
+                ).then(function (campaigns) {
+                    $scope.content.campaigns = campaigns;
+                }).catch(function () {
+                    var c = $scope.content;
+
+                    c.campaigns = defaultCampaigns;
+                    c.campaigns.errorMessage = 'Insufficient permission.';
+                });
+            });
+        };
+
+        $scope.next = function () {
+            $scope.content.campaigns.loading = 1;
+            return fb.getCampaigns(
+                $scope.sessionId,
+                $scope.stateParams.contentId,
+                10,
+                $scope.content.campaigns.offset + 10
+            ).then(function (campaigns) {
+                $scope.content.campaigns = campaigns;
+            }).catch(function () {
+                var c = $scope.content;
+
+                c.campaigns = defaultCampaigns;
+                c.campaigns.errorMessage = 'Insufficient permission.';
+            });
+        };
+
+        $scope.prev = function () {
+            $scope.content.campaigns.loading = 1;
+            return fb.getCampaigns(
+                $scope.sessionId,
+                $scope.stateParams.contentId,
+                10,
+                $scope.content.campaigns.offset - 10
+            ).then(function (campaigns) {
+                $scope.content.campaigns = campaigns;
+            }).catch(function () {
+                var c = $scope.content;
+
+                c.campaigns = defaultCampaigns;
+                c.campaigns.errorMessage = 'Insufficient permission.';
+            });
+        };
 
         function animate () {
             $timeout(function () {
+                $scope.content.campaigns.animate();
                 $scope.sourceStats.forEach(function (stat) {
                     stat.animate();
                 });
@@ -38,8 +143,23 @@ momentum.controller('ContentHistoryController', [
                 'shareStats': content.getShareStatDetails(
                     $scope.sessionId,
                     $scope.stateParams.contentId
+                ),
+                'campaigns': fb.getCampaigns(
+                    $scope.sessionId,
+                    $scope.stateParams.contentId,
+                    10,
+                    0
                 )
             };
+
+            promises.campaigns = promises.campaigns.then(function (data) {
+                $scope.content.campaigns = data;
+            }).catch(function () {
+                var c = $scope.content;
+
+                c.campaigns = defaultCampaigns;
+                c.campaigns.errorMessage = 'Insufficient permission.';
+            });
 
             promises.shareStats = promises.shareStats.then(function (data) {
                 data.forEach(function (datum) {
@@ -97,6 +217,34 @@ momentum.controller('ContentHistoryController', [
                         Math.max(0, stat.offset + dir * 5)
                     );
                 }
+            });
+        };
+
+        $scope.export = function () {
+            var model = {};
+
+            model.months = utils.getMonths();
+
+            return dialog.open({
+                'title': 'Export as CSV',
+                'template': 'monthSelector.tpl.html',
+                'model': model,
+                'showCancel': true,
+                'dialogClass': 'auto-width',
+                'destroy': false
+            }).then(function () {
+                var args = model.months.selected.split('-');
+
+                return fb.export(
+                    $scope.sessionId,
+                    args[1],
+                    args[0],
+                    $scope.stateParams.contentId
+                ).then(function (data) {
+                    utils.downloadCSV(data);
+                });
+            }).finally(function () {
+                dialog.destroy();
             });
         };
 
